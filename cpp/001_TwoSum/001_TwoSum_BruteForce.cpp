c2b8ed74b03bf7b8cfefb3f1728370656becf713
Yongqiang Yu <GeneHit@163.com>
2019-06-01 21:04:39 +0800

    Update 001_TwoSum_BruteForce.cpp

--- cpp/001_TwoSum/001_TwoSum_BruteForce.cpp
+++ cpp/001_TwoSum/001_TwoSum_BruteForce.cpp
@@ -1,1 +1,26 @@
-11
+// Source : https://leetcode.com/problems/two-sum/submissions/
+// Author : Yongqiang Yu
+// Date   : 2019-06-01
+
+/********************************************************************************** 
+* 
+* Design LeetCode class and implement a twoSum function.
+* 
+**********************************************************************************/
+class LeetCode {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unsigned int length=nums.size();
+        vector<int> indices;
+        for(unsigned int i=0;i<length;i++) {
+            for(unsigned int j=i+1;j<length;j++) {
+                if(target == (nums[i]+nums[j])) {
+                    indices.push_back(i);
+                    indices.push_back(j);
+                    return indices;
+                }
+            }
+        }
+        return indices;
+    }
+};
